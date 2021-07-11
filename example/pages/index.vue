5b43525b66167978ab7871e4a6dbdf67020e4630
ecj222 <enochchejieh@gmail.com>
2021-07-11 18:48:58 +0100

    build: added paystack init script

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,7 +1,53 @@
 <template>
-  <div>Works!</div>
+  <button @click="initializePaystack()">
+    Pay With Paystack
+  </button>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Demo',
+  methods: {
+    initializePaystack () {
+      this.$paystack({
+        key: '', // Replace with your public key
+        email: '',
+        amount: 0,
+        ref: '' + Math.floor(Math.random() * 1000000000 + 1),
+        currency: 'NGN',
+        callback: () => {
+          // what you want to do
+        },
+        onClose: () => {
+          // what you want to do.
+        }
+      })
+    }
+  }
+}
 </script>
+
+<style scoped>
+button {
+  display: block;
+  border-radius: 4px;
+  background-color: #08a4da;
+  border: none;
+  color: #fff;
+  text-align: center;
+  font-size: 1.3rem;
+  padding: 14px;
+  width: 300px;
+  -webkit-transition: all 0.5s;
+  transition: all 0.5s;
+  cursor: pointer;
+  position: absolute;
+  top: 48%;
+  left: 50%;
+  transform: translate(-48%, -50%);
+}
+
+button:hover {
+  box-shadow: 0 0px 0px 0 rgba(0, 0, 0, 0.24), 0 1px 10px 0 rgba(0, 0, 0, 0.19);
+}
+</style>
